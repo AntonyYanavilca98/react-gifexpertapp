60f0ff454c472e3e11a669c1a90de8a8a6d5ed9e
Antony <antony.yanavilca98x@gmail.com>
2020-09-13 21:47:45 -0500

    docs agregado

---
+++ docs/precache-manifest.3559220dac3b858d809bc0cc4f55d883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c46172564f1e12e95468e01691677cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2003755f0e187a2e1f4e",
+    "url": "/static/css/main.c5b1d4b2.chunk.css"
+  },
+  {
+    "revision": "92a5aaf936af7a3a2930",
+    "url": "/static/js/2.7c8d2b56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c8d2b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2003755f0e187a2e1f4e",
+    "url": "/static/js/main.48cef9ec.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
